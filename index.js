0fd98a945e0283047d64edf317b33611ebfa437c
millykerry <kerryom975@gmail.com>
2025-10-08 04:31:35 +0300

    Defined a function declaration and a function expression

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// saturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork function expression
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// wrapAdjective function (function returning a function)
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "special") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
+
+// Export for tests
+module.exports = {
+  saturdayFun,
+  mondayWork,
+  wrapAdjective
+};
+
